54573e5dd40bfebcd53fd1a50825a32110af9251
Andrew Bell <andrew.bell@kyndryl.com>
2025-07-15 14:29:54 +0100

    three

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 button = document.getElementById('btn')
+let count = 0 
 
 button.addEventListener('click', () => {
+    count ++
+    button.innerText = count
     console.log('Test')
+    console.log(button.innerText)
 })
